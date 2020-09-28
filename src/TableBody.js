97ce49b794a3dbfc6e39d3d1d52be4a4dcdf51ac
g1st <gintstan@gmail.com>
2020-09-28 09:35:58 +0100

    Show highest scores first

--- src/TableBody.js
+++ src/TableBody.js
@@ -5,9 +5,11 @@ import PlayerScore from './PlayerScore';
 const TableBody = ({ scores }) => {
   return (
     <tbody>
-      {scores.map(({ n, s }, i) => (
-        <PlayerScore key={i} name={n} score={s} />
-      ))}
+      {scores
+        .sort((a, b) => b.s - a.s)
+        .map(({ n, s }, i) => (
+          <PlayerScore key={i} name={n} score={s} />
+        ))}
     </tbody>
   );
 };
