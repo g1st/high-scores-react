e3673577ffa7ab05173c6917fd09a7d7469df6f6
g1st <gintstan@gmail.com>
2020-09-28 09:34:00 +0100

    Present the High Score Tables sorted alphabetically by country name

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ function App() {
   return (
     <div className="App">
       <h1 className="title">High Scores per Country</h1>
-      {scores.map(({ name, scores }, index) => (
-        <HighScoresTable key={index} name={name} scores={scores} />
-      ))}
+      {scores
+        .sort((a, b) => a.name.localeCompare(b.name))
+        .map(({ name, scores }, index) => (
+          <HighScoresTable key={index} name={name} scores={scores} />
+        ))}
     </div>
   );
 }
